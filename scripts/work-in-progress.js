9ddb0f7a0ec39d776dcabed5108b60eacd664fa5
Seweryn Pajor <serwer3103@gmail.com>
2022-04-09 19:34:23 +0200

    Programming courses. Contact information

--- scripts/work-in-progress.js
+++ scripts/work-in-progress.js
@@ -4,6 +4,8 @@ for(link of links) {
 	link.addEventListener('click', workInProgressMenu);
 }
 
-function workInProgressMenu() {
+function workInProgressMenu(event) {
 	alert('Niestety ta podstrona nie jest jeszcze gotowa. Proszę o cierpliwość. Na pewno kiedyś ją zakoduję 👨‍💻');
+    event.preventDefault();
+
 }
