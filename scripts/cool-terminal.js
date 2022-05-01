2d3687bc17b18bee2b5e234e7ea2d083962a81dd
Seweryn Pajor <serwer3103@gmail.com>
2022-05-01 18:39:49 +0200

    Deleted animation for mobile devices

--- scripts/cool-terminal.js
+++ scripts/cool-terminal.js
@@ -1,37 +1,51 @@
 
+function isItMobileDevice() {
 
-const link = document.createElement('link');
-link.href = 'stylesheets/greeting.css';
-link.rel = 'stylesheet';
-document.head.appendChild(link);
+    return navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i) 
+ || navigator.userAgent.match(/iPad/i) 
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+	|| navigator.userAgent.match(/Windows Phone/i);
+}
 
-const background = document.createElement('div');
-background.className = 'terminal-background';
-document.body.appendChild(background);
 
+if(!isItMobileDevice()) {
+    const link = document.createElement('link');
+    link.href = 'stylesheets/greeting.css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+    
+    const background = document.createElement('div');
+    background.className = 'terminal-background';
+    document.body.appendChild(background);
+    
+    
+    
+    const label = document.createElement('div');
+    label.innerText = 'Welcome to sewe-hub.xyz!';
+    label.className = 'greeting';
+    
+    const shellSign = document.createElement('h1');
+    shellSign.innerText = '$ ';
+    shellSign.className = 'shell-sign';
+    background.appendChild(shellSign);
+    
+    background.appendChild(label);
+    
+    const button = document.createElement('button');
+    button.className = 'enter-button';
+    button.innerText = 'Enter website';
+    
+    background.appendChild(button);
+    
+    button.addEventListener('click', () => {
+	document.head.removeChild(link);
+	document.body.removeChild(background);
+    });
+}
 
-
-const label = document.createElement('div');
-label.innerText = 'Welcome to sewe-hub.xyz!';
-label.className = 'greeting';
-
-const shellSign = document.createElement('h1');
-shellSign.innerText = '$ ';
-shellSign.className = 'shell-sign';
-background.appendChild(shellSign);
-
-background.appendChild(label);
-
-const button = document.createElement('button');
-button.className = 'enter-button';
-button.innerText = 'Enter website';
-
-background.appendChild(button);
-
-button.addEventListener('click', () => {
-    document.head.removeChild(link);
-    document.body.removeChild(background);
-});
 
 
 
