26e282c27813eb7f953ab7cea49673cb0ab2663c
sewe2000 <archdevx7d6@sewe-hub.xyz>
2022-10-29 17:37:41 +0200

    Update footer with up-to-date info

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <footer>
-	                <section>
+	        <section>
 			  <h3>Inne moje projekty:</h3>
-			  Zostaw followa na <a href="https://github.com/sewe2000"><i class="fab fa-github"></i></a><br>
-			  <a href="http://git.sewe-hub.xyz">Mój serwer git</a><br>
+			  Zostaw followa na <a href="https://github.com/sewe2000" target="_blank"><i class="fab fa-github"></i></a><br>
+     <a href="http://git.sevedev.com" target="_blank">Mój serwer Gitea</a><br>
 			</section>
 			<section>
 			  <h3>Kontakt:</h3>
-			 <a class="mail" href="mailto:archdevx7d6@sewe-hub.xyz">archdevx7d6@sewe-hub.xyz</a><br>
+			 <a class="mail" href="mailto:s3vdev@proton.me">s3vdev@proton.me</a><br>
 			 <a class="mail" href="mailto:serwer3103@gmail.com">serwer3103@gmail.com</a><br>
 			</section>
 			<section>
 			  <h3>Polecane strony:</h3>
-			  <a href="https://badclup.com">Badclup</a>
+			  <a href="https://w3schools.com" target="_blank">W3Schools</a>
 			</section>
 </footer>
